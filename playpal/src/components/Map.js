6802e59509d1615cb79310afe6c0673bb18c7fcc
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-08-15 15:12:43 -0700

    removing submodule folders test

---
+++ playpal/src/components/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from './Map';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to Playpal</h1>
+            <Map />
+        </div>
+    );
+}
+
+export default App;
